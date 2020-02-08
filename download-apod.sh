3aae044224369b88bd794a9df36892901d52bc8b
Henry <hen@posteo.de>
2020-02-08 22:01:19 +0100

    name file after date

--- download-apod.sh
+++ download-apod.sh
@@ -1,8 +1,10 @@
 address=https://apod.nasa.gov/apod/
 datepattern="$(date +'%Y')"
+filename=$(date +'%Y-%m-%d').jpg
 
 echo $datepattern
 wget -O - $address \
 | awk '/'$datepattern'/{getline; getline; print}' \
-| sed -e  's#^.*\(image.*\.jpg\).*$#'$address'\1#'\
-| xargs wget
+| sed -e 's#^.*\(image.*\.jpg\).*$#'$address'\1#'\
+| xargs wget -O - >> $filename \
+&& echo $filename
