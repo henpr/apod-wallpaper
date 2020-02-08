fdf31e563c7c8081bd7fa67b1bcfd8f1245307d9
hen <hen@posteo.de>
2020-02-08 22:17:15 +0100

    remove debug echo

--- download-apod.sh
+++ download-apod.sh
@@ -2,7 +2,6 @@ address=https://apod.nasa.gov/apod/
 datepattern="$(date +'%Y')"
 filename=$(date +'%Y-%m-%d').jpg
 
-echo $datepattern
 wget -O - $address \
 | awk '/'$datepattern'/{getline; getline; print}' \
 | sed -e 's#^.*\(image.*\.jpg\).*$#'$address'\1#'\
