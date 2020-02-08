bde307f9993907a7f5f4ecc7fedc782ca1c5e7f1
hen <hen@posteo.de>
2020-02-08 22:36:11 +0100

    set wallpaper

--- set-wallpaper.bat
+++ set-wallpaper.bat
@@ -1,1 +1,3 @@
 for /f "usebackq" %%x in (`wsl bash download-apod.sh`) do set APOD_WP_FILENAME=%%x
+
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d C:\Users\hen\workspace\apod-wallpaper\%APOD_WP_FILENAME%
