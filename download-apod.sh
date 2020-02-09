0d6859d293e97d1b17ad5e62e23f3b51184fcfa0
hen <hen@posteo.de>
2020-02-09 21:11:54 +0100

    change wallpaper path, do not download twice

--- download-apod.sh
+++ download-apod.sh
@@ -1,9 +1,13 @@
 address=https://apod.nasa.gov/apod/
 datepattern="$(date +'%Y')"
 filename=$(date +'%Y-%m-%d').jpg
+path=/mnt/c/users/hen/Pictures/
+winpath="C:\\Users\\hen\\Pictures\\"
 
+if [[ ! -f "$path$filename" ]]; then
 wget -O - $address \
-| awk '/'$datepattern'/{getline; getline; print}' \
-| sed -e 's#^.*\(image.*\.jpg\).*$#'$address'\1#'\
-| xargs wget -O - >> $filename \
-&& echo $filename
+    | awk '/'$datepattern'/{getline; getline; print}' \
+    | sed -e 's#^.*\(image.*\.jpg\).*$#'$address'\1#'\
+    | xargs wget -O - >> $path$filename 
+fi
+echo $winpath$filename
--- set-wallpaper.bat
+++ set-wallpaper.bat
@@ -1,3 +1,4 @@
 for /f "usebackq" %%x in (`wsl bash download-apod.sh`) do set APOD_WP_FILENAME=%%x
 
-reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d C:\Users\hen\workspace\apod-wallpaper\%APOD_WP_FILENAME%
+reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d %APOD_WP_FILENAME%
+RUNDLL32.EXE user32.dll,UpdatePerUserSystemParameters
