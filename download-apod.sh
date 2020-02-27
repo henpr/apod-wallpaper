4792584a9ef9915185a3230ba38022b220199b9e
hpr <henry.preusser@jat-gmbh.de>
2020-02-27 12:10:54 +0100

    correct sed string

--- download-apod.sh
+++ download-apod.sh
@@ -1,16 +1,18 @@
 address=https://apod.nasa.gov/apod/
 datepattern="$(date +'%Y')"
 filename=$(date +'%Y-%m-%d').jpg
-path=/mnt/c/users/hen/Pictures/
-winpath="C:\\Users\\hen\\Pictures\\"
-
-if [[ ! -f "$path$filename" ]]; then
-wget -O - $address \
+path=/mnt/d/HenryP/Pictures/apod
+winpath="d:/henryp/pictures/apod"
+test='<a href="image/2002/LaSillaLaPalma_Horalek_Casado_ZLCircle_viz_1500px.png">'
+echo $test | sed -e 's#.*\(image.*\(\.png\|\.jpg\)\).*#'$address'\1#'
+#if [[ ! -f "$path$filename" ]]; then
+path=$(wget -O - $address \
     | awk '/'$datepattern'/{getline; getline; print}' \
-    | sed -e 's#^.*\(image.*\.jpg\).*$#'$address'\1#'\
-    | xargs wget -O - >> $path$filename 
-fi
+    | sed -e 's#.*\(image.*\.png\).*#\1#'\
+    | xargs echo) # wget -O - >> $path$filename 
+echo $path
+#fi
 # if run on windows
-echo $winpath$filename
+# echo $winpath/$filename
 # in run on linux
-xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set $path$filename
+# xfconf-query --channel xfce4-desktop --property /backdrop/screen0/monitor0/image-path --set $path$filename
--- set-wallpaper.bat
+++ set-wallpaper.bat
@@ -2,3 +2,4 @@ for /f "usebackq" %%x in (`wsl bash download-apod.sh`) do set APOD_WP_FILENAME=%
 
 reg add "HKCU\Control Panel\Desktop" /v Wallpaper /f /t REG_SZ /d %APOD_WP_FILENAME%
 call RUNDLL32.EXE user32.dll, UpdatePerUserSystemParameters
+pause
